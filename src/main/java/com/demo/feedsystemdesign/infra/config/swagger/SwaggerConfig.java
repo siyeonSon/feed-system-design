07ceecc13ad46a0e44d9960048df74c847715879
siyeonSon <siyeon0209@naver.com>
2024-09-23 16:01:58 +0900

    config: swagger 설정 추가

---
+++ src/main/java/com/demo/feedsystemdesign/infra/config/swagger/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.demo.feedsystemdesign.infra.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value(value = "${swagger.server-url}")
+    private String serverUrl;
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Feed System Design API")
+                        .description("Feed System Design API 명세서입니다.")
+                        .version("v0.0.1"))
+                .servers(List.of(new Server().url(serverUrl)));
+    }
+
+}
