52b5b6a78a112c75551c5cf739004c6a1529a966
siyeonSon <siyeon0209@naver.com>
2024-09-23 15:15:33 +0900

    initial commit

---
+++ src/main/java/com/demo/feedsystemdesign/FeedSystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.demo.feedsystemdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedSystemDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedSystemDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/feedsystemdesign/FeedSystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdomo.feedsystemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedSystemDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
