e8cddbf0711c6a39dc22cfbb693eaade130227a8
siyeonSon <siyeon0209@naver.com>
2024-09-23 15:52:33 +0900

    feat: redis 키 상수 선언

---
+++ src/main/java/com/demo/feedsystemdesign/infra/cache/constant/CacheKeyConstant.java
@@ -0,0 +1,13 @@
+package com.demo.feedsystemdesign.infra.cache.constant;
+
+public final class CacheKeyConstant {
+    public static final String FOLLOWER_IDS_CACHE_KEY = "follower_ids:";
+    public static final String FEED_IDS_CACHE_KEY = "feed_ids:";
+    public static final String POST_INFO_CACHE_KEY = "post_info:";
+    public static final String USER_INFO_CACHE_KEY = "user_info:";
+    public static final String IS_INFLUENCER_CACHE_KEY = "is_influencer:";
+    public static final String INFLUENCER_POST_CACHE_KEY = "influencer_post:";
+
+    private CacheKeyConstant() {
+    }
+}
