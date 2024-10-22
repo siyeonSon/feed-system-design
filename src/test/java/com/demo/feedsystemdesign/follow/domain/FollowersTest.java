b0de16453590768ff8946146f5195a338aaa36bf
0chil <0@chll.it>
2024-10-22 21:02:08 +0900

    feat(follower): 팔로우 여부를 알 수 있다

--- src/test/java/com/demo/feedsystemdesign/follow/domain/FollowersTest.java
+++ src/test/java/com/demo/feedsystemdesign/follow/domain/FollowersTest.java
@@ -1,7 +1,8 @@
 package com.demo.feedsystemdesign.follow.domain;
 
-import com.demo.feedsystemdesign.user.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,11 +11,20 @@ class FollowersTest {
     @Test
     void 다른_사용자가_팔로우_할_수_있다() {
         Followers followers = new Followers();
-        User other = new User();
 
-        followers.add(other.getId());
+        followers.add(1L);
 
-        assertThat(followers.contains(other.getId())).isTrue();
+        assertThat(followers.contains(1L)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, true", "0, false"})
+    void 팔로우_여부를_알_수_있다(Long otherUserId, boolean follows) {
+        Followers followers = new Followers();
+
+        followers.add(1L);
+
+        assertThat(followers.contains(otherUserId)).isEqualTo(follows);
     }
 
     @Test
