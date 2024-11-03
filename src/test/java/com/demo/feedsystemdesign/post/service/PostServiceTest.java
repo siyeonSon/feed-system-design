5bf14d96580f6170a002dc166122ce29c7c4d44e
siyeonSon <siyeon0209@naver.com>
2024-11-03 11:38:55 +0900

    chore: 불필요한 어노테이션 제거

--- src/test/java/com/demo/feedsystemdesign/post/service/PostServiceTest.java
+++ src/test/java/com/demo/feedsystemdesign/post/service/PostServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest
 class PostServiceTest {
 
