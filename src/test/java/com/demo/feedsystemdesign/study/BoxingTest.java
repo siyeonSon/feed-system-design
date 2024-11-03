1128176d47243b710eb90441fa4219cefea7ed00
0chil <0@chll.it>
2024-11-03 18:00:04 +0900

    feat(study): 레퍼런스 타입과 원시 타입 박싱 학습 테스트 보완

--- src/test/java/com/demo/feedsystemdesign/study/BoxingTest.java
+++ src/test/java/com/demo/feedsystemdesign/study/BoxingTest.java
@@ -1,5 +1,7 @@
 package com.demo.feedsystemdesign.study;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -7,32 +9,50 @@ import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class BoxingTest {
 
     @Test
-    void 십만개를_박싱_및_언박싱한다() {
-        Duration unboxingAndBoxing = time(() -> {
-            Long a = 0L;
-            for (long i = 0; i < 100_000; i++) {
-                a = a + i;  // 더하는 연산에서 언박싱 발생 -> a 대입 과정에서 박싱 발생
-            }
-        });
-        Duration noBoxing = time(() -> {
+    void 박싱이_없는_경우가_가장_빠르며_박싱_및_언박싱이_거듭될수록_느려진다() {
+        Duration neverBox = time(() -> {
             long a = 0L;
             for (long i = 0; i < 100_000; i++) {
                 a = a + i;  // 박싱과 언박싱 없음
             }
         });
-        Duration unboxingBothAndBoxing = time(() -> {
+        Duration unboxAndBox = time(() -> {
+            Long a = 0L;
+            for (long i = 0; i < 100_000; i++) {
+                a = a + i;  // 더하는 연산에서 언박싱 발생 -> a 대입 과정에서 박싱 발생
+            }
+        });
+        Duration unboxBothThenBox = time(() -> {
             Long a = 0L;
             for (Long i = 0L; i < 100_000; i++) {
                 a = a + i;  // 더하는 연산에서 두 변수 모두 언박싱 발생 -> a 대입 과정에서 박싱 발생
             }
         });
 
-        assertThat(noBoxing).isLessThan(unboxingAndBoxing);
-        assertThat(unboxingAndBoxing).isLessThan(unboxingBothAndBoxing);
+        assertThat(neverBox).isLessThan(unboxAndBox);
+        assertThat(unboxAndBox).isLessThan(unboxBothThenBox);
+    }
 
+    @Test
+    void 가장_느린_경우와_가장_빠른_경우에도_유의미한_차이는_없다() {
+        Duration operationsWithoutBoxing = time(() -> {
+            long a = 0L;
+            for (long i = 0; i < 100_000; i++) {
+                a = a + i;
+            }
+        });
+        Duration operationsWithUnboxingAndBoxing = time(() -> {
+            Long a = 0L;
+            for (Long i = 0L; i < 100_000; i++) {
+                a = a + i;
+            }
+        });
+
+        assertThat(operationsWithoutBoxing).isCloseTo(operationsWithUnboxingAndBoxing, Duration.ofMillis(10));
     }
 
 
