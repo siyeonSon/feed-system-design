eedb050c5d3d923c8815ddfb376e3e7b2eecddee
siyeonSon <siyeon0209@naver.com>
2024-11-13 19:25:18 +0900

    config: Async 설정

---
+++ src/main/java/com/demo/feedsystemdesign/infra/config/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.demo.feedsystemdesign.infra.config.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
