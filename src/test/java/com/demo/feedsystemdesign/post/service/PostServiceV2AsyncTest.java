0c043129bd636ba170bec270ef0801bbed6a60ea
siyeonSon <siyeon0209@naver.com>
2024-11-13 17:21:52 +0900

    test(feed): 이벤트 발행 테스트 성공

--- src/test/java/com/demo/feedsystemdesign/post/service/PostServiceV2AsyncTest.java
+++ src/test/java/com/demo/feedsystemdesign/post/service/PostServiceV2AsyncTest.java
@@ -1,40 +1,38 @@
 package com.demo.feedsystemdesign.post.service;
 
 import com.demo.feedsystemdesign.follow.service.FollowService;
+import com.demo.feedsystemdesign.post.event.PostCreatedEvent;
 import com.demo.feedsystemdesign.user.domain.User;
 import com.demo.feedsystemdesign.user.domain.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RecordApplicationEvents
 class PostServiceV2AsyncTest {
 
-    @SpyBean
+    @Autowired
     private PostServiceV2Async postService;
 
-    @SpyBean
+    @Autowired
     private UserRepository userRepository;
 
-    @SpyBean
+    @Autowired
     private FollowService followService;
 
-    @MockBean
-    private ApplicationEventPublisher eventPublisher;
-
     @Test
-    void 사용자가_게시물을_작성하면_게시물_작성_이벤트가_발행된다() {
+    void 사용자가_게시물을_작성하면_게시물_작성_이벤트가_발행된다(ApplicationEvents events) {
         User user = userRepository.save(new User());
         User follower = userRepository.save(new User());
         followService.follow(follower.getId(), user.getId());
-
         postService.createPost(user.getId(), "test content");
 
-        verify(eventPublisher).publishEvent(any());
+        assertThat(events.stream(PostCreatedEvent.class).count())
+                        .isEqualTo(1);
     }
 }
